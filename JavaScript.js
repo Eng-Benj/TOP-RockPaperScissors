6f5101cceae105579865b72547456e585026520e
Eng-Benj <119880823+Eng-Benj@users.noreply.github.com>
2023-01-09 15:02:19 +0000

    Add function 'getComputerChoice' to JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random()*3);
+    let computerChoice
+    if (randomNumber === 0) {
+        computerChoice = "Rock"
+    }
+    else if (randomNumber === 1) {
+        computerChoice = "Paper"
+    }
+    else if (randomNumber === 2) {
+        computerChoice = "Scissors"
+    }
+    else {computerChoice = "Problem"}
+    console.log(randomNumber)
+    console.log(computerChoice)
+}
+
+getComputerChoice()
