d38c014c37c9e4afe402ca79067318ea478f4496
Eng-Benj <119880823+Eng-Benj@users.noreply.github.com>
2023-01-09 16:51:00 +0000

    Add functions 'playRound' and 'game' to JavaScript.js which produce a 5-round game of rps in the console

--- JavaScript.js
+++ JavaScript.js
@@ -1,18 +1,42 @@
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
-    let computerChoice
+    let computerSelection
     if (randomNumber === 0) {
-        computerChoice = "Rock"
+        computerSelection = "rock"
     }
     else if (randomNumber === 1) {
-        computerChoice = "Paper"
+        computerSelection = "paper"
     }
     else if (randomNumber === 2) {
-        computerChoice = "Scissors"
+        computerSelection = "scissors"
     }
-    else {computerChoice = "Problem"}
-    console.log(randomNumber)
-    console.log(computerChoice)
+    return computerSelection
 }
 
-getComputerChoice()
+function playRound(playerSelection , computerSelection) {
+    let outcome
+    if (playerSelection == null || playerSelection.toLowerCase() != ("rock" || "paper" || "scissors")) {console.log("You failed to show rock, paper, or scissors"); outcome = 0}
+    else if (playerSelection.toLowerCase() === computerSelection) {
+        console.log("You both chose " + playerSelection.toLowerCase() + "!"); outcome = 1
+    } 
+    else if ((playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") || (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") || (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper"))  {
+        console.log("Your " + playerSelection.toLowerCase() + " beats the computer's " + computerSelection + "!"); outcome = 2
+    }
+    else { 
+        console.log("The computer's " + computerSelection + " beats your " + playerSelection.toLowerCase() + "!"); outcome = 0
+    }
+}
+
+function game() {
+    let score = 0, result
+    for (let i = 0; i < 5; i++ ){
+    score += playRound(prompt("Round " + (i + 1) + " - choose your weapon!"), getComputerChoice())
+    }
+    if (score == 5 ){result = "draw"; console.log("It's a draw!")}
+    else if (score > 5){result = "win"; console.log("You win!")}
+    else {result = "loss"; console.log("You lose!")}
+    let replay = prompt("Do you want to play again? (y/n)")
+    if (replay === "y"){game()}
+}
+
+game()
